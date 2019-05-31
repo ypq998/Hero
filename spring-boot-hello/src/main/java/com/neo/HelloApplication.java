15100a121fd1f2fbadfaade661536f31601cc1d3
sz_jhtc30 <sz_jhtc30@V-WYM-216.jieshun.com.cn>
2019-05-31 11:24:03 +0800

    for the third

--- spring-boot-hello/src/main/java/com/neo/HelloApplication.java
+++ spring-boot-hello/src/main/java/com/neo/HelloApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloApplication {
 // for test only
 	// for the second
+	// for the third
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
 	}
